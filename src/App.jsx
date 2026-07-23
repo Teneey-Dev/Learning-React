a4dee3ebc8e6cc041fcdc99a6615aadc37a94933
Awodire - Teniola <awodireteniola4@gmail.com>
2026-07-23 21:07:20 +0100

    feat: integrate Appwrite for movie search tracking and trending movies
    - Added Appwrite SDK and implemented functions to update search counts and fetch trending movies.
    - Introduced debouncing for search input to optimize API calls.
    - Updated App component to display trending movies and handle search term updates.
    - Removed unused assets and SVG files to clean up the project.
    - Updated package.json to include new dependencies for Appwrite and react-use.
    - Adjusted Vite configuration for proper base path.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = "https://api.themoviedb.org/3"; 
 
@@ -21,9 +23,15 @@ const App = () => {
  const [searchTerm, setSearchTerm] = useState("");
 const [errorMessage, setErrorMessage] = useState('');
 const [movieList, setMovieList] = useState([]);
+const [trendingMovies, setTrendingMovies] = useState([])
 const [isLoading, setIsLoading] = useState(false);
+const [debouncedSearchTerm, setDebouncedSearhTerm] = useState("")
 
  
+useDebounce (()=> setDebouncedSearhTerm(searchTerm), 500, [searchTerm])
+
+
+
 const fetchMovies = async(query = '') => {
   setIsLoading(true)
   setErrorMessage("")
@@ -47,6 +55,12 @@ if(data.Response === false) {
 
 setMovieList(data.results || []);
 
+
+
+if (query && data.results.length > 0) {
+  await updateSearchCount(query, data.results[0]);
+}
+
   } catch (error) {
     console.error(`Error fetching movies: ${error}`);
     setErrorMessage("Error fetching movies. Please try again later")
@@ -55,20 +69,50 @@ setMovieList(data.results || []);
   }
 }
 
+const loadTrendingMovies = async () => {
+  try {
+    const movies = await getTrendingMovies()
+
+    setTrendingMovies(movies) 
+  } catch (error) {
+        console.error(`Error fetching movies: ${error}`);
+  }
+}
+
   useEffect(() => {
-fetchMovies(searchTerm)
-}, [searchTerm ]);
+fetchMovies(debouncedSearchTerm)
+}, [debouncedSearchTerm]);
+
+useEffect(() => {
+  loadTrendingMovies()
+}, [])
 
   return (
     <main>
    <div className="Pattern"/> 
    <div className="Wrapper"> 
    <header>
-    <img src="./hero.png" alt="Hero Banner" />
+<img src="./hero.png" alt="" />
     <h1>Find <span className='text-gradient'>Movies</span> You"ll Enjoy without the Hassle</h1>
   
    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </header>
+
+{trendingMovies.length > 0 && (
+  <section className='trending'>
+    <h2>Trending Movies</h2>
+
+    <ul>
+      {trendingMovies.map((movie, index) => (
+       <li key={movie.$id}>
+        <p>{index + 1}</p>
+        <img src={movie.poster_url} alt={movie.title} />
+       </li>
+      ))}
+    </ul>
+  </section>
+)}
+
   <section className='all-movies'>
     <h2 className='mt-[20px]'>All Movies</h2>
 
@@ -81,6 +125,8 @@ fetchMovies(searchTerm)
         {movieList.map((movie) => (
 <MovieCard key={movie.id} movie={movie} />
         ))}
+
+
       </ul>
    )}
 
